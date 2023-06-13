0d7894320dd506859b78819d806a3e4233283e29
rajenrediffmail <waghmarerajendra007@rediffmail.com>
2023-06-13 11:11:33 +0530

    Change pakage name

---
+++ src/com/coreJava/exceptionsConceptRules/NegativeSalaryException.java
@@ -0,0 +1,12 @@
+/**
+ * Created by WrajendraG on 2/9/2017.
+ */
+package com.coreJava.exceptionsConceptRules;
+
+public class NegativeSalaryException extends Exception {
+	public NegativeSalaryException(String s) {
+		super(s);
+
+	}
+
+}
